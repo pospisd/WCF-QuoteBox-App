e3e53b5046f9c6b2e4a0f54a4b8d3eacc3018286
Robert Clark <bobclark1976@hotmail.com>
2019-08-10 13:37:43 +1000

    Added Entity Framework repo for Authors and class for classic ADO.Net DataAdapters.

---
+++ QuoteBoxData/DALEF/QuoteBoxDALDC.cs
@@ -0,0 +1,114 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace QuoteBoxData.DALEF
+{
+    class QuoteBoxDALDC
+    {
+        /// <summary>
+        /// Models individual tables in QuoteBox, namely "Authors" and "Quotes".
+        /// </summary>
+        /// 
+
+        //----------------------------------------------------------------------------
+        // Disconnected Layer - Data Adapter.
+        //
+        // Data Adapter objects function as a bridge between the client tier and a 
+        // relational database. Using these objects, you can obtain DataSet objects,
+        // manipulate their contents, and send the modified rows back for processing. 
+        // The end result is a highly scalable data-centric .NET application.
+        //
+        // Notice the connection to the database does not need to be explicitly opened 
+        // or closed. Also, DataSets were designed for simple forms over data business 
+        // applications.
+        //
+        // If your DataTable maps to or is generated from a single database table, you 
+        // can take advantage of the DbCommandBuilder object to automatically 
+        // generate the DeleteCommand, InsertCommand, and UpdateCommand objects for the 
+        // DataAdapter.
+        //
+        //----------------------------------------------------------------------------
+        private string ConnectionString;
+        private SqlDataAdapter Adapter = null;
+
+        /// <summary>
+        /// Public constructor.
+        /// </summary>
+        /// <param name="queryString"></param>
+        /// <param name="connectionString"></param>
+        public QuoteBoxDALDC(string queryString, string connectionString)
+        {
+            this.ConnectionString = connectionString;
+            ConfigureAdapter(queryString, out this.Adapter);
+        }
+
+        /// <summary>
+        /// Assign the sql commands to the adapter. (Takes advantage of SqlCommandBuilder.)
+        /// Best to provision each instance of QuoteBoxDALDC to query one table only.
+        /// </summary>
+        /// <param name="sqlStatement"></param>
+        /// <param name="adapter"></param>
+        private void ConfigureAdapter(string sqlStatement, out SqlDataAdapter adapter)
+        {
+            // Create the adapter and set up the SelectCommand.
+            adapter = new SqlDataAdapter(sqlStatement, ConnectionString);
+
+            // Obtain the remaining command objects at runtime using the SqlCommandBuilder.
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+        }
+
+        /// <summary>
+        /// Adds or refreshes rows in the DataSet to match those in the datasource.
+        /// (With option to use FillSchema.)
+        /// </summary>
+        /// <param name="addSchema"></param>
+        /// <param name="ds"></param>
+        /// <param name="tableName"></param>
+        public void FillAdapter(bool addSchema, ref DataSet ds, string tableName)
+        {
+            // Byt default the table schema is not copied across with Fill method.
+            if (addSchema.Equals(true))
+            {
+                Adapter.FillSchema(ds, SchemaType.Source, tableName);
+            }
+            Adapter.Fill(ds, tableName);
+        }
+
+        /// <summary>
+        /// The correct command object is leveraged behind the scenes.
+        /// </summary>
+        /// <param name="dt"></param>
+        public void UpdateAdapter(DataTable dt)
+        {
+            Adapter.Update(dt);
+        }
+
+        //----------------------------------------------------------------------------
+        // SERIALIZE:
+        //----------------------------------------------------------------------------
+        /// <summary>
+        /// Serialize DataTable/DataSet objects in binary format.
+        /// </summary>
+        /// <param name="quotesDS"></param>
+        static void SaveAndLoadAsBinary(DataSet quotesDS)
+        {
+            // Set binay serialization flag.
+            quotesDS.RemotingFormat = SerializationFormat.Binary;
+
+            // Save this DataSet as binary.
+            FileStream fs = new FileStream("BinaryQuotes.bin", FileMode.Create);
+            BinaryFormatter bFormat = new BinaryFormatter();
+            bFormat.Serialize(fs, quotesDS);
+            fs.Close();
+
+            // Clear out DataSet.
+            quotesDS.Clear();
+
+            // Load DataSet from binary file.
+            fs = new FileStream("BinaryQuotes.bin", FileMode.Open);
+            DataSet data = (DataSet)bFormat.Deserialize(fs);
+        }
+    }
+}
---
+++ QuoteBoxData/Repos/AuthorRepo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using QuoteBoxData.EF;
+
+namespace QuoteBoxData.Repos
+{
+    public class AuthorRepo
+    {
+        private QuoteBoxEntities Context;
+
+        public AuthorRepo(QuoteBoxEntities context)
+        {
+            this.Context = context;
+        }
+
+        public IEnumerable<Author> GetAuthors()
+        {
+            return Context.Authors.ToList();
+        }
+
+        public Author GetAuthorByID(int id)
+        {
+            return Context.Authors.Find(id);
+        }
+
+        public void InsertAuthor(Author author)
+        {
+            Context.Authors.Add(author);
+        }
+
+        public void DeleteAuthor(int authorID)
+        {
+            Author author = Context.Authors.Find(authorID);
+            Context.Authors.Remove(author);
+        }
+
+        public void UpdateAuthor(Author author)
+        {
+            Context.Entry(author).State = System.Data.Entity.EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            Context.SaveChanges();
+        }
+
+        private bool dispose = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.dispose)
+            {
+                if (disposing)
+                {
+                    Context.Dispose();
+                }
+            }
+            this.dispose = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
